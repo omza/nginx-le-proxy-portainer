601a4e14a9af3d8a37ae476dcb8459c3e37fa98e
Oliver Meyer zu Altenschildesche <omza@Olivers-Mac-Pro.fritz.box>
2019-09-04 16:58:21 +0200

    mkdir if not exisit

--- start.sh
+++ start.sh
@@ -6,7 +6,14 @@ else
     exit 1
 fi
 
-# 2. Substitute env vars in config files and copy to 
+# 2. Make Directories if not exists
+mkdir -p echo ${PROXY_DATA_LOCATION}/portainer
+mkdir -p echo ${PROXY_DATA_LOCATION}/sites-enabled
+mkdir -p echo ${PROXY_DATA_LOCATION}/snippets
+mkdir -p echo ${PROXY_DATA_LOCATION}/letsencrypt
+
+# 2. Substitute env vars in config files and copy to
+export ${CERT1_SERVER_NAME}
 envsubst \${CERT1_SERVER_NAME}<./nginx-server-portainer.conf
 cp -f ./nginx-server-portainer.conf echo ${PROXY_DATA_LOCATION}/sites-enabled
 
